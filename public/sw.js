bc9a37a9fb13d60f89994c5a13bc55d72f1f07c6
reginald <ddrbnsn@gmail.com>
2021-11-21 11:06:08 -0800

    sw fxs

--- public/sw.js
+++ public/sw.js
@@ -1,32 +1,17 @@
-
 self.addEventListener('install', e => {
     e.waitUntil(
-      caches.open('mempwa').then(cache => {
-        console.log("added")
-        return cache.addAll([
-            '/',
-            '/index.html',
-            '/index.js',
-            '/style.css',
-            '/assets',
-            '/manifest.json',
-          ]);
-      })      
+        caches.open('mempwa').then(cache => {
+            console.log("adding")
+            return cache.addAll([
+                '/',
+                'index.html',
+                'index.js',
+                'style.css',
+                'assets',
+                'manifest.json',
+            ]);
+        })
     );
-   });
+});
 
-self.addEventListener('fetch', e =>{
-    console.log("event",e)
-    console.log("caches.match", caches.match(e.request))
-    e.respondWith(woah(e) || noah(e))
-})
-
-function woah(e){
-    console.log("wpah", caches.match(e.request))
-    return caches.match(e.request)
-}
-
-function noah(e){
-    console.log("npah", fetch(e.request))
-    return fetch(e.request)
-}
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request) || fetch(e.request)))
